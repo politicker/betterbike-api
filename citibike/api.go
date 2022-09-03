2c2d58cd9800433f04e3ab25c86c189991f2d4ab
Harrison Borges <hborges005@gmail.com>
2022-09-03 12:27:08 -0400

    trim api response

--- citibike/api.go
+++ citibike/api.go
@@ -20,24 +20,14 @@ type APIResponse struct {
 					Lat float64 `json:"lat"`
 					Lng float64 `json:"lng"`
 				} `json:"location"`
-				BikesAvailable          int           `json:"bikesAvailable"`
-				BikeDocksAvailable      int           `json:"bikeDocksAvailable"`
-				EbikesAvailable         int           `json:"ebikesAvailable"`
-				ScootersAvailable       int           `json:"scootersAvailable"`
-				TotalBikesAvailable     int           `json:"totalBikesAvailable"`
-				TotalRideablesAvailable int           `json:"totalRideablesAvailable"`
-				IsValet                 bool          `json:"isValet"`
-				IsOffline               bool          `json:"isOffline"`
-				IsLightweight           bool          `json:"isLightweight"`
-				DisplayMessages         []string      `json:"displayMessages"`
-				SiteId                  string        `json:"siteId"`
-				Ebikes                  []Ebike       `json:"ebikes"`
-				Scooters                []interface{} `json:"scooters"`
-				LastUpdatedMs           int64         `json:"lastUpdatedMs"`
+				BikesAvailable      int     `json:"bikesAvailable"`
+				BikeDocksAvailable  int     `json:"bikeDocksAvailable"`
+				EbikesAvailable     int     `json:"ebikesAvailable"`
+				TotalBikesAvailable int     `json:"totalBikesAvailable"`
+				IsOffline           bool    `json:"isOffline"`
+				Ebikes              []Ebike `json:"ebikes"`
+				LastUpdatedMs       int64   `json:"lastUpdatedMs"`
 			} `json:"stations"`
-			Rideables     []interface{} `json:"rideables"`
-			Notices       []interface{} `json:"notices"`
-			RequestErrors []interface{} `json:"requestErrors"`
 		} `json:"supply"`
 	} `json:"data"`
 }
