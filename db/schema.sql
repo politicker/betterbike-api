e7f9847ea4f0a2e78aa190dc9ca806c957724aee
Harrison Borges <hborges005@gmail.com>
2023-05-15 00:26:42 -0400

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -18,10 +18,11 @@ create table if not exists stations_timeseries
     name                 text      not null,
     lat                  float     not null,
     lon                  float     not null,
+    bikes_available      int       not null default 0,
     ebikes_available     int       not null default 0,
     bike_docks_available int       not null default 0,
-    ebikes               jsonb     not null default '{}',
     last_updated_ms      bigint    not null,
+    is_offline           boolean   not null default false,
     created_at           timestamp not null default now(),
     PRIMARY KEY (id, last_updated_ms)
 );
