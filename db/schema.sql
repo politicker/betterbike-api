2f0806c57804429717d0d737632a025eefd04ce4
Harrison Borges <hborges005@gmail.com>
2024-03-12 11:00:37 -0400

    upgrade to sqlc v2

--- db/schema.sql
+++ db/schema.sql
@@ -1,28 +1,28 @@
 CREATE EXTENSION if not exists postgis;
 
-create table if not exists stations
-(
-    id                   text primary key,
-    name                 text      not null,
-    lat                  float     not null,
-    lon                  float     not null,
-    ebikes_available     int       not null default 0,
-    bike_docks_available int       not null default 0,
-    ebikes               jsonb     not null default '{}',
-    created_at           timestamp not null default now()
-);
+create table
+    if not exists stations (
+        id text primary key,
+        name text not null,
+        lat float not null,
+        lon float not null,
+        ebikes_available int not null default 0,
+        bike_docks_available int not null default 0,
+        ebikes jsonb not null default '{}',
+        created_at timestamp not null default now ()
+    );
 
-create table if not exists stations_timeseries
-(
-    id                   text      not null,
-    name                 text      not null,
-    lat                  float     not null,
-    lon                  float     not null,
-    bikes_available      int       not null default 0,
-    ebikes_available     int       not null default 0,
-    bike_docks_available int       not null default 0,
-    last_updated_ms      bigint    not null,
-    is_offline           boolean   not null default false,
-    created_at           timestamp not null default now(),
-    PRIMARY KEY (id, last_updated_ms)
-);
+create table
+    if not exists stations_timeseries (
+        id text not null,
+        name text not null,
+        lat float not null,
+        lon float not null,
+        bikes_available int not null default 0,
+        ebikes_available int not null default 0,
+        bike_docks_available int not null default 0,
+        last_updated_ms bigint not null,
+        is_offline boolean not null default false,
+        created_at timestamp not null default now (),
+        PRIMARY KEY (id, last_updated_ms)
+    );
