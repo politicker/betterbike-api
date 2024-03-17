1e5142d0b1b47c303a9d95e7d6005cdd6cafdcc0
Harrison Borges <hborges005@gmail.com>
2024-03-16 21:25:13 -0400

    handle singular case

--- internal/domain/util.go
+++ internal/domain/util.go
@@ -16,10 +16,19 @@ func timeAgo(t time.Time) string {
 
 	switch {
 	case seconds < 60:
+		if seconds == 1 {
+			return "1 second ago"
+		}
 		return fmt.Sprintf("%d seconds ago", seconds)
 	case minutes < 60:
+		if minutes == 1 {
+			return "1 minute ago"
+		}
 		return fmt.Sprintf("%d minutes ago", minutes)
 	case hours < 24:
+		if hours == 1 {
+			return "1 hour ago"
+		}
 		return fmt.Sprintf("%d hours ago", hours)
 	default:
 		return fmt.Sprintf("%d days ago", days)
